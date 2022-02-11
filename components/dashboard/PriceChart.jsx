143f269d2507fe4012021ce8033c43445d520c84
noob77777 <abhishekpal7@outlook.com>
2022-02-11 13:23:47 +0530

    :add: dashboard  chart

---
+++ components/dashboard/PriceChart.jsx
@@ -0,0 +1,229 @@
+import {
+  Button,
+  Grid,
+  ToggleButton,
+  ToggleButtonGroup,
+  Typography,
+} from '@mui/material';
+import Link from '@components/MuiNextLink';
+import {
+  VictoryChart,
+  VictoryLine,
+  VictoryAxis,
+  VictoryTooltip,
+  VictoryVoronoiContainer,
+} from 'victory';
+import theme from '@styles/theme';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import axios from 'axios';
+
+const stepUnitMapper = {
+  '1h': {
+    stepSize: 1,
+    stepUnit: 'h',
+  },
+  '4h': {
+    stepSize: 4,
+    stepUnit: 'h',
+  },
+  '1d': {
+    stepSize: 1,
+    stepUnit: 'd',
+  },
+  '1w': {
+    stepSize: 1,
+    stepUnit: 'w',
+  },
+  '1m': {
+    stepSize: 1,
+    stepUnit: 'm',
+  },
+};
+
+const pairBaseCurrencyMapper = {
+  ergopad_sigusd: '$',
+  ergopad_erg: 'erg',
+};
+
+const initHistoryData = {
+  token: 'xyzpad_erg',
+  resolution: 10,
+  history: [
+    {
+      timestamp: new Date().toISOString(),
+      price: 1,
+    },
+    {
+      timestamp: new Date(0).toISOString(),
+      price: 0,
+    },
+  ],
+};
+
+const PriceChart = () => {
+  const [rawData, setRawData] = useState(initHistoryData);
+  const [stepUnit, setStepUnit] = useState('1h');
+  const [pair, setPair] = useState('ergopad_sigusd');
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const res = await axios.get(
+          `${process.env.API_URL}/asset/price/chart/${pair}?stepSize=${stepUnitMapper[stepUnit].stepSize}&stepUnit=${stepUnitMapper[stepUnit].stepUnit}`
+        );
+        setRawData(res.data);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+
+    getData();
+  }, [stepUnit, pair]);
+
+  const priceData = rawData.history.map((dataPoint) => {
+    return {
+      x: dataPoint.timestamp,
+      y: dataPoint.price,
+    };
+  });
+
+  const handleStepChange = (e, newAlignment) => {
+    if (newAlignment !== null) {
+      setStepUnit(newAlignment);
+    }
+  };
+
+  const handlePairChange = (e, newAlignment) => {
+    if (newAlignment !== null) {
+      setPair(newAlignment);
+    }
+  };
+
+  return (
+    <>
+      <Typography variant="h4">$ERGOPAD</Typography>
+      <Grid>
+        <Grid container>
+          <Grid item md={6}>
+            <ToggleButtonGroup
+              color="info"
+              value={pair}
+              exclusive
+              onChange={handlePairChange}
+              sx={{ mb: 3, mt: 0 }}
+            >
+              <ToggleButton value="ergopad_sigusd">SigUSD</ToggleButton>
+              <ToggleButton value="ergopad_erg">Erg</ToggleButton>
+            </ToggleButtonGroup>
+          </Grid>
+          <Grid
+            item
+            md={6}
+            sx={{ display: 'flex', justifyContent: 'flex-end' }}
+          >
+            <ToggleButtonGroup
+              color="info"
+              value={stepUnit}
+              exclusive
+              onChange={handleStepChange}
+              sx={{ mb: 3, mt: 0 }}
+            >
+              <ToggleButton value="1h">1 hour</ToggleButton>
+              <ToggleButton value="4h">4 hours</ToggleButton>
+              <ToggleButton value="1d">1 day</ToggleButton>
+              <ToggleButton value="1w">1 week</ToggleButton>
+              {/* <ToggleButton value="1m">1 month</ToggleButton> */}
+            </ToggleButtonGroup>
+          </Grid>
+        </Grid>
+        <VictoryChart
+          height={150}
+          padding={{ top: 5, bottom: 30, right: 30, left: 40 }}
+          domainPadding={{ y: 15 }}
+          containerComponent={
+            <VictoryVoronoiContainer
+              id="victory-stack-chart-container"
+              style={{
+                touchAction: 'auto',
+              }}
+              labelComponent={<VictoryTooltip style={{ fontSize: 6 }} />}
+              labels={(d) => {
+                return (
+                  Math.floor(d.datum.y * 10000) / 10000 +
+                  pairBaseCurrencyMapper[pair]
+                );
+              }}
+            />
+          }
+        >
+          <VictoryLine
+            style={{
+              data: { stroke: theme.palette.tertiary.main },
+            }}
+            animate={{ easing: 'exp' }}
+            interpolation="natural"
+            data={priceData}
+          />
+          <VictoryAxis
+            dependentAxis
+            tickFormat={(price) => {
+              return price + pairBaseCurrencyMapper[pair];
+            }}
+            style={{
+              axis: { stroke: 'white' },
+              ticks: { stroke: 'white' },
+              tickLabels: { fill: 'white', fontSize: 6 },
+            }}
+          />
+          <VictoryAxis
+            fixLabelOverlap
+            crossAxis
+            invertAxis
+            tickFormat={(timestamp) => {
+              return new Date(Date.parse(timestamp)).toDateString();
+            }}
+            style={{
+              axis: { stroke: 'white' },
+              ticks: { stroke: 'white' },
+              tickLabels: { fill: 'white', fontSize: 7 },
+            }}
+          />
+        </VictoryChart>
+        <Grid sx={{ display: 'flex', justifyContent: 'flex-end' }}>
+          <Link
+            underline="none"
+            href={'https://app.ergodex.io/swap'}
+            aria-label="ergodex"
+            title="Trade"
+            rel="noreferrer"
+            target="_blank"
+            sx={{ mb: 3, mx: 3 }}
+          >
+            <Button
+              variant="contained"
+              sx={{
+                color: '#fff',
+                fontSize: '1rem',
+                py: '0.6rem',
+                px: '1.6rem',
+                backgroundColor: theme.palette.tertiary.main,
+                '&:hover': {
+                  backgroundColor: theme.palette.tertiary.hover,
+                  boxShadow: 'none',
+                },
+                '&:active': {
+                  backgroundColor: theme.palette.tertiary.active,
+                },
+              }}
+            >
+              Trade
+            </Button>
+          </Link>
+        </Grid>
+      </Grid>
+    </>
+  );
+};
+
+export default PriceChart;
