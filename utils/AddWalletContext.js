cfbfdfeb2b3439def81edc5b58e097face60e4a7
noob77777 <abhishekpal7@outlook.com>
2022-02-14 10:32:45 +0530

    :improve: ux for add wallet

--- utils/AddWalletContext.js
+++ utils/AddWalletContext.js
@@ -1,50 +1,66 @@
-import { createContext, useContext, useState } from 'react'
+import { createContext, useContext, useState } from 'react';
 
-// The Context 
+// The Context
 const AddWalletContext = createContext();
 
 // Template Provider
-const AddWalletProvider = ({children}) => {
+const AddWalletProvider = ({ children }) => {
+  const [addWalletOpen, setAddWalletOpenRaw] = useState(false);
 
-    const [addWalletOpen, setAddWalletOpen] = useState(false);
+  const setAddWalletOpen = (newState) => {
+    /**
+     * wrapper for setAddWalletOpen to refresh dApp connection
+     */
+    setAddWalletOpenRaw(newState);
+    if (newState) {
+      try {
+        // refresh connection
+        if (localStorage.getItem('dapp_connected')) {
+          window.ergo_check_read_access().then((res) => {
+            if (!res) window.ergo_request_read_access();
+          });
+        }
+      } catch (e) {
+        console.log(e);
+      }
+    }
+  };
 
-    // Context values passed to consumer
-    const value = {
-        addWalletOpen,    // <------ Expose Value to Consumer
-        setAddWalletOpen  // <------ Expose Setter to Consumer
-    };
+  // Context values passed to consumer
+  const value = {
+    addWalletOpen, // <------ Expose Value to Consumer
+    setAddWalletOpen, // <------ Expose Setter to Consumer
+  };
 
-    return (
-        <AddWalletContext.Provider value={value}>
-            {children}
-        </AddWalletContext.Provider>
-    )
-}
+  return (
+    <AddWalletContext.Provider value={value}>
+      {children}
+    </AddWalletContext.Provider>
+  );
+};
 
 // Template Consumer
-const AddWalletConsumer = ({children}) => {
-    return (
-        <AddWalletContext.Consumer>
-            {(context) => {
-                if (context === undefined) {
-                    throw new Error('AddWalletConsumer must be used within AddWalletProvider');
-                }
-                return children(context)
-            }}
-        </AddWalletContext.Consumer>
-    )
-}
+const AddWalletConsumer = ({ children }) => {
+  return (
+    <AddWalletContext.Consumer>
+      {(context) => {
+        if (context === undefined) {
+          throw new Error(
+            'AddWalletConsumer must be used within AddWalletProvider'
+          );
+        }
+        return children(context);
+      }}
+    </AddWalletContext.Consumer>
+  );
+};
 
 // useTemplate Hook
 const useAddWallet = () => {
-    const context = useContext(AddWalletContext);
-    if(context === undefined)
-        throw new Error('useAddWallet must be used within AddWalletProvider');
-    return context;
-}
+  const context = useContext(AddWalletContext);
+  if (context === undefined)
+    throw new Error('useAddWallet must be used within AddWalletProvider');
+  return context;
+};
 
-export {
-    AddWalletProvider,
-    AddWalletConsumer,
-    useAddWallet
-}
+export { AddWalletProvider, AddWalletConsumer, useAddWallet };
